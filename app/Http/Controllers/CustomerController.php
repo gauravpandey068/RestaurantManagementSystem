ca5b46b413a654132b1501f285c70bb7f543c5de
Gaurav Pandey <pandeygaurav068@gmail.com>
2022-02-03 09:02:21 +0545

    Customer Model

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('waiterRole');
+    }
+
+    public function index()
+    {
+        return view('customer.index');
+    }
 }
--- database/migrations/2022_02_03_085337_create_customers_table.php
+++ database/migrations/2022_02_03_085337_create_customers_table.php
@@ -17,6 +17,7 @@ class CreateCustomersTable extends Migration
             $table->id();
             $table->string('name');
             $table->integer('table_no');
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
