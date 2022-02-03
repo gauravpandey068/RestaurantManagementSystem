37ae84dec29016cbbfef2c6c6ed048c1b73bbab9
Gaurav Pandey <pandeygaurav068@gmail.com>
2022-02-03 09:02:47 +0545

    Customer Model

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Customer extends Model
 {
     use HasFactory;
-    protected $fillable= ['name', 'table_no'];
+    protected $fillable= ['name', 'table_no', 'is_active'];
 }
