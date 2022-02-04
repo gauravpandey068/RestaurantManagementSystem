1ed3cabc2092d394d8597572040f8c94f7a65317
Gaurav Pandey <pandeygaurav068@gmail.com>
2022-02-04 10:43:55 +0545

    Payment Model and migrations

---
+++ app/Models/Payment.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['customer_id','total_price', 'is_paid', 'total_price'];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+}
